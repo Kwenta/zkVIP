b700dd4acd146d7343c5f2bdf4a7847776990b79
Elliott He <elliott.he@outlook.com>
2024-04-29 09:57:11 +0800

    contract impl

--- contract/contracts/FeeReimbursementApp.sol
+++ contract/contracts/FeeReimbursementApp.sol
@@ -8,12 +8,18 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "brevis-contracts/contracts/sdk/apps/framework/BrevisApp.sol";
 import "brevis-contracts/contracts/sdk/interface/IBrevisProof.sol";
 
+interface IAccountModule {
+    function getAccountOwner(uint128 accountId) external view returns (address);
+}
 
 contract FeeReimbursementApp is BrevisApp, Ownable {
     using SafeERC20 for IERC20;
-    
+
+    address public rewardToken;
+    uint24 public rewardTokenDecimals;
+    IAccountModule public accountModule;
     bytes32 public vkHash;
-    event FeeReimbursed(address indexed user, uint24 tradeYearMonth, uint256 fee);
+    event FeeReimbursed(address indexed user, uint128 accountId, uint24 tradeYearMonth, uint248 feeRebate);
 
     constructor(address _brevisProof) BrevisApp(IBrevisProof(_brevisProof)) {}
 
@@ -27,18 +33,46 @@ contract FeeReimbursementApp is BrevisApp, Ownable {
         // our designated verifying key. This proves that the _circuitOutput is authentic
         require(vkHash == _vkHash, "invalid vk");
 
-        (address user, uint24 tradeYearMonth, uint256 fee) = decodeOutput(_circuitOutput);
-        emit FeeReimbursed(user, tradeYearMonth, fee);
+        (uint24 yearMonth, uint128 accountId, uint248 volume, uint248 fee) = decodeOutput(_circuitOutput);
+        uint248 feeRebate;
+        if (volume > 100000000 * 1e18) {
+            feeRebate = fee * 90 / 100;
+        } else if (volume > 10000000 * 1e18) {
+            feeRebate = fee * 75 / 100;
+        } else if (volume > 1000000 * 1e18) {
+            feeRebate = fee * 50 / 100;
+        } else if (volume > 100000 * 1e18) {
+            feeRebate = fee * 20 / 100;
+        }
+
+        address user;
+        if (feeRebate > 0) {
+            user = accountModule.getAccountOwner(accountId);
+            if (user != address(0)) {
+                uint256 feeInRewardToken = feeRebate * (1 ** rewardTokenDecimals) / 1e18;
+                IERC20(rewardToken).safeTransfer(user, feeInRewardToken);
+            }
+        }
+        emit FeeReimbursed(user, accountId, yearMonth, feeRebate);
     }
 
-    function decodeOutput(bytes calldata o) internal pure returns (address, uint24, uint256) {
-        address user = address(bytes20(o[0:20]));
-        uint24 tradeYearMonth = uint24(bytes3(o[20:23])); 
-        uint256 fee = uint256(uint248(bytes31(o[23:54]))); 
-        return (user, tradeYearMonth, fee);
+    function decodeOutput(bytes calldata o) internal pure returns (uint24 yearMonth, uint128 accountId, uint248 volume, uint248 fee) {
+        yearMonth = uint24(bytes3(o[16:19]));
+        accountId = uint128(bytes16(o[19:35]));
+        volume = uint248(bytes31(o[35:66]));
+        fee = uint248(bytes31(o[66:97]));
     }
 
     function setVkHash(bytes32 _vkHash) external onlyOwner {
         vkHash = _vkHash;
+    }
+
+    function setRewardToken(address _rewardToken, uint24 _decimals) external onlyOwner {
+        rewardToken = _rewardToken;
+        rewardTokenDecimals = _decimals;
+    }
+
+    function setAccountModule(IAccountModule _accountModule) external onlyOwner {
+        accountModule = _accountModule;
     }
 }
