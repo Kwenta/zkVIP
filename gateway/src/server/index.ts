1868fd54ae6e7a8c6d21e6451235c6bd87d93f19
jinyao <1650091775@qq.com>
2024-06-05 17:15:15 +0800

    add more logs

--- gateway/src/server/index.ts
+++ gateway/src/server/index.ts
@@ -50,62 +50,104 @@ setInterval(prepareUserTradeVolumeFees, 2000);
 monitorFeeAccumulated();
 monitorBrevisRequest();
 
-// prepareNewDayTradeClaims();
+prepareNewDayTradeClaims();
 // setInterval(prepareUserTradeVolumeFees, 60000);
 
 submitUserSwapAmountTx();
 setInterval(submitUserSwapAmountTx, 1000);
 
 
-getAllTradesWithin30Day(1714608000, 1717246799).then(result => {
-  const tsStart = 1717200000
-  const tsEnd = 1717246799
-  console.log(`result: ${result.trades.length}, err: ${result.error}`)
-  const accountTradesMap = getAccountTradesMap(result.trades)
-  console.log(`accountTradesMap: ${accountTradesMap}`)
+var deleteDay = 1
+a()
+setInterval(a, 30000)
 
-  type TradesInfo = {
-    tradeLength: number,
-    unclaimableLength: number,
-    claimableLength: number,
-  }
-  const tradesInfos: TradesInfo[] = []
-  for (let [account, trades] of accountTradesMap) {      
-    if (trades.length === 0) {
-      tradesInfos.push({
-        tradeLength: 0,
-        unclaimableLength: 0,
-        claimableLength: 0,
+async function a() {
+  const yesterday = Number((moment.utc(new Date()).subtract(deleteDay, "d")).format('YYYYMMDD'))
+  const yesterdayStart = moment.utc(yesterday.toString(), "YYYYMMDD", true)
+  const tsStart = yesterdayStart.utc().unix()
+  const tsEnd = yesterdayStart.utc().add(1, "d").unix() - 1
+  const ts30DAgo = yesterdayStart.utc().subtract(29, "d").unix()
+
+  getAllTradesWithin30Day(ts30DAgo, tsEnd).then(result => {
+    console.log(`result: ${result.trades.length}, err: ${result.error}`)
+    const accountTradesMap = getAccountTradesMap(result.trades)
+    console.log(`accountTradesMap: ${accountTradesMap}`)
+  
+    type TradesInfo = {
+      tradeLength: number,
+      unclaimableLength: number,
+      claimableLength: number,
+    }
+    const tradesInfos: TradesInfo[] = []
+    for (let [account, trades] of accountTradesMap) {      
+      if (trades.length === 0) {
+        tradesInfos.push({
+          tradeLength: 0,
+          unclaimableLength: 0,
+          claimableLength: 0,
+        })
+        continue
+      }
+  
+      const claimableTrades = trades.filter(trade => {
+        return trade.timestamp >= tsStart && trade.timestamp <= tsEnd
       })
-      continue
+  
+      tradesInfos.push({
+        tradeLength: trades.length,
+        claimableLength: claimableTrades.length,
+        unclaimableLength: trades.length-claimableTrades.length,
+      })
+      // console.log(`account ${account} claimable trades: ${claimableTrades.length} and unclaimable trades ${trades.length-claimableTrades.length}`)
     }
-
-    const claimableTrades = trades.filter(trade => {
-      return trade.timestamp >= tsStart && trade.timestamp <= tsEnd
+  
+    tradesInfos.sort((a,b) => {
+      if (a.tradeLength > b.tradeLength) {
+        return -1
+      } else if (a.tradeLength == b.tradeLength && a.claimableLength > b.claimableLength) {
+        return -1
+      } else {
+        return 1
+      }
     })
 
-    tradesInfos.push({
-      tradeLength: trades.length,
-      claimableLength: claimableTrades.length,
-      unclaimableLength: trades.length-claimableTrades.length,
-    })
-    // console.log(`account ${account} claimable trades: ${claimableTrades.length} and unclaimable trades ${trades.length-claimableTrades.length}`)
-  }
 
-  tradesInfos.sort((a,b) => {
-    if (a.tradeLength > b.tradeLength) {
-      return -1
-    } else if (a.tradeLength == b.tradeLength && a.claimableLength > b.claimableLength) {
-      return -1
-    } else {
-      return 1
-    }
+    const smallCircuit = tradesInfos.filter(info => {
+      return info.claimableLength <= 20 && info.unclaimableLength <= 216
+    }).length
+
+    const mCircuit = tradesInfos.filter(info => {
+      return info.claimableLength <= 50 && info.unclaimableLength <= 412
+    }).length
+
+    const lCircuit = tradesInfos.filter(info => {
+      return info.claimableLength <= 300
+    }).length
+
+    const notSupport = tradesInfos.filter(info => {
+      return info.claimableLength > 300
+    }).length
+
+    console.log(`${yesterday}: tradesInfos.length: ${tradesInfos.length},
+     smallCircuit support: ${smallCircuit} coverage: ${ smallCircuit/tradesInfos.length}
+     mediumCircuit: ${mCircuit- smallCircuit} coverage: ${ mCircuit/tradesInfos.length}
+     lCircuit: ${lCircuit- mCircuit} coverage: ${ lCircuit/tradesInfos.length}
+     not support: ${notSupport} percentage: ${ notSupport/tradesInfos.length}
+     `)
+
+  
+    // for (var i = 0; i < 20; i++) {
+    //   if (i < tradesInfos.length ) {
+    //     const tradesInfo = tradesInfos[i]
+    //     console.log(`trades ${tradesInfo.tradeLength} claimable: ${tradesInfo.claimableLength} and unclaimable: ${tradesInfo.unclaimableLength}`)  
+    //   }
+     
+    // }
+    console.log(`=========================================================================================`)
+
+  }).catch(error => {
+    console.error("get all trades within 30day error", error)
   })
+}
 
-  tradesInfos.forEach(tradesInfo => {
-    console.log(`trades ${tradesInfo.tradeLength} claimable: ${tradesInfo.claimableLength} and unclaimable: ${tradesInfo.unclaimableLength}`)
-  })
 
-}).catch(error => {
-  console.error("get all trades within 30day error", error)
-})
