dda8b98b80b3dbb621b5fc0252d207c5cc7cab1f
jinyao <1650091775@qq.com>
2024-06-25 14:16:27 +0800

    deploy mock fee module

--- contract/contracts/MockFeeModule.sol
+++ contract/contracts/MockFeeModule.sol
@@ -4,9 +4,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MockFeeModule is Ownable {
     function getFeeRebatePercentage(uint248 volume30D) external pure returns (uint64) {
-        if (volume30D > 30000) {
-            return 2;
-        }
-        return 1;
+        uint248 _volume = volume30D / 1000000000000000000;
+        if (_volume > 1000000000) {
+            return 30;
+        } else  if (_volume > 100000000) {
+            return 20;
+        } else  if (_volume > 10000000) {
+            return 10;
+        }  else  if (_volume > 1000000) {
+            return 5;
+        } 
+        return 0;
     }
 }
