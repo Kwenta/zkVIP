ebf9aa65049c3ea4e4f3d7c4319427601e4d10fc
Flocqst <flocastet@icloud.com>
2024-07-30 14:44:55 +0200

    ✅ Adjust tests to refactor + fuzz some existing tests

--- contract/test/FeeReimbursementClaim.t.sol
+++ contract/test/FeeReimbursementClaim.t.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-import { Test, console } from "forge-std/Test.sol";
-import { FeeReimbursementClaim } from "../contracts/ClaimRebate.sol";
-import { FeeReimbursementApp } from "../contracts/FeeReimbursementApp.sol";
-import { MockERC20 } from "./mocks/MockERC20.sol";
-import { MockFactory } from "./mocks/MockFactory.sol";
+import {Test} from "forge-std/Test.sol";
+import {FeeReimbursementClaim} from "../contracts/FeeReimbursementClaim.sol";
+import {FeeReimbursementApp} from "../contracts/FeeReimbursementApp.sol";
+import {MockERC20} from "./mocks/MockERC20.sol";
+import {MockFactory} from "./mocks/MockFactory.sol";
 import {AggregatorV2V3Interface} from
     "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV2V3Interface.sol";
 
@@ -14,7 +14,7 @@ contract FeeReimbursementClaimTest is Test {
     FeeReimbursementApp public mockFeeReimbursementApp;
     MockERC20 public mockRewardToken;
     MockFactory public mockFactory;
-    
+
     AggregatorV2V3Interface public opDataFeed;
     AggregatorV2V3Interface public opSequencerUptimeFeed;
 
@@ -22,23 +22,25 @@ contract FeeReimbursementClaimTest is Test {
     address public user1 = address(0x234);
     address public account = address(0x456);
     address public smartMarginAccount = address(0x789);
+    address public mockAddress = address(0x345);
 
     uint256 constant TEST_VALUE = 1 ether;
-    
+
     function setUp() public {
         mockFeeReimbursementApp = new FeeReimbursementApp(address(0x345));
         mockFactory = new MockFactory();
         mockRewardToken = new MockERC20();
-        opDataFeed = AggregatorV2V3Interface(0x0D276FC14719f9292D5C1eA2198673d1f4269246);
-        opSequencerUptimeFeed = AggregatorV2V3Interface(0x371EAD81c9102C9BF4874A9075FFFf170F2Ee389);
-        
+        opDataFeed = AggregatorV2V3Interface(mockAddress);
+        opSequencerUptimeFeed = AggregatorV2V3Interface(mockAddress);
+
         // Deploy the contract
         vm.prank(owner);
         feeReimbursementClaim = new FeeReimbursementClaim(
             address(mockFeeReimbursementApp),
             address(mockFactory),
             address(mockRewardToken),
-            address(opDataFeed)
+            address(opDataFeed),
+            address(opSequencerUptimeFeed)
         );
 
         // Set up initial conditions
@@ -46,22 +48,39 @@ contract FeeReimbursementClaimTest is Test {
         mockRewardToken.mint(address(feeReimbursementClaim), TEST_VALUE);
     }
 
-    function testUpdateBlacklist() public {
-        vm.prank(owner);
-        feeReimbursementClaim.updateBlacklist(account, true);
+    function test_Update_Blacklist(address caller) public {
+        vm.startPrank(caller);
 
-        // Check if the account is blacklisted
-        assertTrue(feeReimbursementClaim.blacklist(account));
+        if (caller == owner) {
+            feeReimbursementClaim.updateBlacklist(account, true);
+
+            assertTrue(feeReimbursementClaim.blacklist(account));
+        } else {
+            vm.expectRevert("Ownable: caller is not the owner");
+            feeReimbursementClaim.updateBlacklist(account, true);
+
+            assertFalse(feeReimbursementClaim.blacklist(account));
+        }
+
+        vm.stopPrank();
     }
 
-    function test_Only_Owner_Can_Call_recoverERC20() public {
-        vm.prank(user1);
-        vm.expectRevert("Ownable: caller is not the owner");
-        feeReimbursementClaim.recoverERC20(address(mockRewardToken), 0);
+    function test_Only_Owner_Can_Call_recoverERC20(address caller) public {
+        vm.startPrank(caller);
+
+        if (caller == owner) {
+            feeReimbursementClaim.recoverERC20(address(mockRewardToken), 0);
+        } else {
+            vm.expectRevert("Ownable: caller is not the owner");
+            feeReimbursementClaim.recoverERC20(address(mockRewardToken), 0);
+        }
+
+        vm.stopPrank();
     }
 
-    function test_Can_Recover_ERC20_Token() public {
+    function test_Can_RecoverERC20_Token() public {
         assertEq(mockRewardToken.balanceOf(address(feeReimbursementClaim)), TEST_VALUE);
+        assertEq(mockRewardToken.balanceOf(address(owner)), 0);
 
         vm.prank(owner);
         feeReimbursementClaim.recoverERC20(address(mockRewardToken), TEST_VALUE);
--- contract/test/mocks/MockFactory.sol
+++ contract/test/mocks/MockFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import { IFactory } from "../../contracts/ClaimRebate.sol";
+import {IFactory} from "../../contracts/interfaces/IFactory.sol";
 
 contract MockFactory is IFactory {
     // Mapping to store account owners
