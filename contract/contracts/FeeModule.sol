6c65d5b20dadf509fa1f0c7d33602e828747d5a5
Flocqst <flocastet@icloud.com>
2024-08-29 13:09:41 +0200

    👷 nit : unused import

--- contract/contracts/FeeModule.sol
+++ contract/contracts/FeeModule.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract FeeModule is Ownable {
+contract FeeModule {
     function getFeeRebatePercentage(uint248 volume30D) external pure returns (uint64) {
         if (volume30D > 1_000_000_000 * 1 ether) {
             return 30;
