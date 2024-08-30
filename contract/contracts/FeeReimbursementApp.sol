d45bb73735d0e6387a16b6f1a1a7c640ca3c5f58
Flocqst <flocastet@icloud.com>
2024-08-30 15:50:33 +0200

    👷 remove unused imports

--- contract/contracts/FeeReimbursementApp.sol
+++ contract/contracts/FeeReimbursementApp.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "brevis-contracts/contracts/sdk/apps/framework/BrevisApp.sol";
 import "brevis-contracts/contracts/sdk/interface/IBrevisProof.sol";
@@ -24,8 +22,6 @@ error onlyClaimContractCanAccess();
 error ZeroAddress();
 
 contract FeeReimbursementApp is BrevisApp, Ownable {
-    using SafeERC20 for IERC20;
-
     IFeeRebateTierModule public feeRebateTierModule;
     uint256 public contractsHash;
     mapping(bytes32 => uint16) public vkHashesToCircuitSize; // batch tier vk hashes => tier batch size
